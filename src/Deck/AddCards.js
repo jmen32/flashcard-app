e18a031f77630842cdb49fa7083dc3819d803487
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-08 23:12:44 -0500

    creates addCard button to deck directory

---
+++ src/Deck/AddCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddCards() {
+  return (
+      <div>
+        <button className= "btn btn-primary mr-2">
+        <span className='oi oi-plus'></span> Add Cards</button>
+        </div>
+  )
+}
