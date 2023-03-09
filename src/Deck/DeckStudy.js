b4997eca8b6e99ca77eb5f2460ff7f392a230ad7
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-08 23:17:22 -0500

    adds edit button to Deck directory

---
+++ src/Deck/DeckStudy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useHistory, useParams} from 'react-router-dom'
+
+export default function DeckStudy() {
+const history = useHistory();
+const {deckId} = useParams();
+
+  return (
+    <div>
+      <button className="btn btn-primary mr-2" onClick={() => history.push(`/decks/${deckId}/edit`)}><span className='oi oi-book mx-1'></span>Study</button>
+    </div>
+  )
+}
