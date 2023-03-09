fb26e255193db40ff5b7eab8140be503525a564b
Jon Mendoza <jonmendoza1824@gmail.com>
2023-03-08 23:14:54 -0500

    renders deleteButtont to Deck directory

---
+++ src/Deck/DeckDelete.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function DeckDelete() {
+  
+  return (
+      <div className="d-flex justify-content-end">
+      <button className="btn btn-darks float-right">
+        <span className='oi oi-trash'></span> Delete</button>
+    </div>
+  )
+}
+
